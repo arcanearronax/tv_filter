e8f376117dcfa89d46538f885ea1c5a1d552b6ac
arcanearronax <arcanearronax@gmail.com>
2019-10-23 22:24:55 -0400

    Finishing the current version of apiclient. After this touching up views so some testing can be performed.

--- tvapi/tvapi/apiclient.py
+++ tvapi/tvapi/apiclient.py
@@ -4,20 +4,17 @@ import urllib.parse
 import logging
 from bs4 import BeautifulSoup
 import re
+from structures import *
 
 logger = logging.getLogger('apilog')
 
-base_uri = 'https://api.themoviedb.org/3/'
+tmdb_uri = 'https://api.themoviedb.org/3/'
 api_key = 'c5b9f96015c7b3e771a36a2b43159f6d'
 language = 'en-US'
 
 class APIClient():
-
-	show_names = {}
-
-	show_imdb_ids = {}
-
-	ep_imdb_ids = {}
+	# Shared Values
+	shows = Season()
 
 	# Used to encode url queries
 	def url_encode(value):
@@ -26,78 +23,61 @@ class APIClient():
 	@classmethod
 	def get_show_name(cls,show_id):
 		try:
-			show_name = cls.show_names['{}'.format(show_id)]
+			show_name = cls.shows['{}'.format(show_id)]
 		except KeyError:
-			url = '{}tv/{}?api_key={}&language={}'.format(base_uri, \
-				show_id,api_key, language)
+			url = '{}tv/{}?api_key={}&language={}'.format(tmdb_uri,show_id,api_key,language)
 			req = requests.get(url)
 			json_data = json.loads(req.text)
 			show_name = json_data['name']
 			cls.show_names['{}'.format(show_id)] = show_name
-		
+
 		return show_name
 
 	@classmethod
 	def find_show(cls,value):
 		logger.info('find_show: {}'.format(value))
-		
-		url = '{}search/tv?api_key={}&language={}&query={}'.format(base_uri, \
+		url = '{}search/tv?api_key={}&language={}&query={}'.format(tmdb_uri, \
                 api_key, language, APIClient.url_encode(value))
 		req = requests.get(url)
 		json_data = json.loads(req.text)
-		show_name = json_data['results'][0]['original_name']
-		show_id = json_data['results'][0]['id']
-
-		cls.show_names['{}'.format(show_id)] = show_name
-		
+		tmdb_name = json_data['results'][0]['original_name']
+		tmdb_id = json_data['results'][0]['id']
+		#cls.show_names['{}'.format(tmdb_id)] = tmdb_name
+		cls.shows.set(tmdb_id,tmdb_name)
 		logger.info('tvmb_id: {}'.format(show_id))
 		return show_id
 
 	@classmethod
-	def find_show_by_id(cls,show_id):
-		logger.info('find_show_by_id: {}'.format(show_id))
-			
-		return {
-			'show_name': cls.get_show_name(show_id)
-		}
-
-	@classmethod
 	def find_seasons(cls,show_id):
 		logger.info('find_seasons: {}'.format(show_id))
-
-		url = '{}tv/{}?api_key={}&language={}'.format(base_uri, \
-            show_id, api_key, language)
-
+		url = '{}tv/{}?api_key={}&language={}'.format(tmdb_uri, show_id, api_key, language)
 		req = requests.get(url)
-		
 		json_data = json.loads(req.text)
+		season = Season()
+		season_cnt = int(json_data['number_of_seasons'])
 
-		seasons = json_data['number_of_seasons']
+		for i in range(1,season_cnt+1):
+			cls.shows.add_season(show_id,i,season_cnt)
 
-		return {
-			'seasons': seasons,
-			'show_name': cls.get_show_name(show_id),
-			}
+		return season_cnt
 
 	@classmethod
 	def get_episodes(cls,show_id,season):
 		logger.info('get_episodes: {} - {}'.format(show_id,season))
-
-		url = '{}tv/{}/season/{}?api_key={}&language={}'.format(base_uri, \
-			show_id, season, api_key, language)
-
+		url = '{}tv/{}/season/{}?api_key={}&language={}'.format(tmdb_uri, show_id, season, api_key, language)
 		req = requests.get(url)
-
 		json_data = json.loads(req.text)
+		episodes = json_data['episodes']
 
-		episodes = len(json_data['episodes'])
+		for e in episodes:
+			ep_num = e['episode_number']
+			tmdb_id = e['id']
+			tmdb_name = e['name']
+			episode = Episode()
+			episode.set(ep_num,tmdb_id=tmbd_id,tmdb_name=tmdb_name)
+			cls.shows.set_episode(show_id,season,ep_num,episode)
 
-		context = {'episodes': episodes}
-
-		return {
-			'episodes': episodes,
-			'show_name': cls.get_show_name(show_id),
-		}
+		return len(episodes)
 
 	@classmethod
 	def get_show_imdb_id(cls, show_id):
@@ -107,61 +87,69 @@ class APIClient():
 		try:
 			ret = cls.show_imdb_ids['{}'.format(show_id)]
 		except KeyError:
-			url = '{}'.format('https://movie-database-imdb-alternative.p.rapidapi.com/')
 
+			url = '{}'.format('https://movie-database-imdb-alternative.p.rapidapi.com/')
 			headers = {
 				'x-rapid-host': 'movie-database-imdb-alternative.p.rapidapi.com',
 				'x-rapidapi-key': '7f282ba72amsh62fa1b3b0be127bp1fce1fjsn38c68fb682ad',
 			}
-
 			parameters = {
 				'page': '1',
 				'r': 'json',
 				's': cls.get_show_name(show_id),
-		}
-
+			}
 			req = requests.get(url,headers=headers,params=parameters)
-			#logger.info('req - {}'.format(req.text))
-			json_data = json.loads(req.text)
-			#logger.info('json - {}'.format(json_data))
-	
-			ret = json_data['Search'][0]['imdbID']
-			#logger.info('ret - '.format(ret))
 
-			cls.show_imdb_ids['{}'.format(show_id)] = ret 
-			
+			json_data = json.loads(req.text)
+			ret = json_data['Search'][0]['imdbID']
+			cls.shows.set(show_id,imdb_id=ret)
+
 		return ret
 
 	@classmethod
-	def get_show_imdb_season_list(cls, show_imdb_id, season):
+	def get_season_imdb_info(cls,show_tmdb_id,season_num):
+		logger.info('get_season_imdb_info: {}-{}'.format(show_tmdb_id, season_num))
+
+		show_imdb_id = cls.shows.get(show_tmdb_id)['imdb_id']
 		url = 'https://www.imdb.com/title/{}/episodes?season={}'.format(show_imdb_id,season)
-		soup = BeautifulSoup(url)
+		page = requests.get(url)
+		assert page.status_code == 200, 'Failed to retrieve page'
 
-		episode_div = soup.findAll('div', {'class': 'list_detail_eplist'})
+		soup = BeautifulSoup(page.content, 'html.parser')
+		eps_div_odd = soup.find('div',{'class':'list_item odd'})
+		eps_div_even = soup.find('div',{'class':'list_item odd'})
+		eps_div_full = ep_div_odd + ep_div_even
 
-		logger.info('episode_div - {}'.format(episode_div))
+		for ep in ep_div_full:
+			image_div = ep.find('div',{'class':'image'})
+			a_elem = image_div.find('a')
 
-		
+			a_div = a_elem.find('div')
+			div_cont = a_div.find('div').text
+
+			ep_num = re.search('[\d]*$',div_cont).group(0)
+			logger.info('ep_num: {}'.format(ep_num))
+
+			hover_div = a_elem.find('div',{'class':'hover-over-image'})
+			ep_imdb_id = hover_div['data-const']
+			logger.info('emp_imdb_id: {}'.format(ep_imdb_id))
+
+			cls.shows.set_episode(show_tmdb_id,season_num,ep_num,imdb_id,imdb_name)
+
+		return self[str(show_tmdb_id)]['seasons'][str(season_num)]
+
 
 	@classmethod
-	def get_episode_cast(cls,show_id,season,episode):
+	def get_episode_cast(cls,show_id,season,ep_id):
 		logger.info('get_episode_cast: {} - {} - {}'.format(show_id,season,episode))
+		str_show_id = str(show_id)
+		str_season = str(season)
+		str_ep_id = str(ep_id)
 
-		try:
-			cls.ep_imdb_ids[str(show_id)]
-		except KeyError:
-			cls.ep_imdb_ids.update({str(show_id):{str(season):{}}})
-		else:
-			try:
-				cls.ep_imdb_ids[str(show_id)][str(season)]
-			except KeyError:
-				cls.ep_imdb_ids[str(show_id)][str(season)] = {}
-			
-		
-		show_imdb_id = cls.get_show_imdb_id(show_id)
+		show_imdb_id = cls.get_show_imdb_id(str_show_id)
 		logger.info('found_show_imdb_id: {}'.format(show_imdb_id))
 
-		url = 'https://www.imdb.com/title/{}/episodes?season={}'.format(show_imdb_id,season)
+		url = 'https://www.imdb.com/title/{}/episodes?season={}'.format(show_imdb_id,str_season)
 		logger.info('SEASON_URL: {}'.format(url))
 
 		page = requests.get(url)
@@ -192,21 +180,12 @@ class APIClient():
 
 		ep_content = art_list.find('div',{'id':'episodes_content'})
 		logger.info('7- {}'.format(ep_content.__class__))
-		#logger.info('7- {}'.format(str(ep_content)[:2000]))
 
 		clear_div = ep_content.find('div',{'itemtype':'http://schema.org/TVSeason'})
 		logger.info('8- {}'.format(clear_div.__class__))
-		#logger.info('8 - {}'.format(clear_div.text))
 
 		ep_list_div = clear_div.find('div', {'class':'list detail eplist'})
 		logger.info('9- {}'.format(ep_list_div.__class__))
-
-		#logger.info('LISTING --- {}'.format(ep_list_div))
-
-		#for ep in ep_list_div:
-		#	logger.info('ep - {}'.format(ep[:50]))
-
-		logger.info('TESTING')
 
 		ep_list_odd = ep_list_div.find_all('div', {'class': 'list_item odd'})
 		logger.info('10- {}'.format(ep_list_odd.__class__))
@@ -235,14 +214,14 @@ class APIClient():
 			logger.info('ep_num: {}'.format(ep_num))
 
 			hover_div = a_elem.find('div',{'class':'hover-over-image'})
-			ep_imdb_id = hover_div['data-const']		
+			ep_imdb_id = hover_div['data-const']
 			logger.info('emp_imdb_id: {}'.format(ep_imdb_id))
 
 			# Build the json for the series' season
 			ep_imdb_ids[str(show_id)][str(season)][str(ep_num)] = str(ep_imdb_id)
 
 		logger.info('ep_imdb_ids - {}'.format(ep_imdb_ids))
-		
+
 		cls.ep_imdb_ids[str(show_id)][str(season)] = ep_imdb_ids[str(show_id)][str(season)]
 		logger.info('CLASS ------- {}'.format(cls.ep_imdb_ids))
 
@@ -250,9 +229,9 @@ class APIClient():
 		#
 		# Now we get the specified episode's cast page
 		#
-		
 
-		epid = cls.ep_imdb_ids[str(show_id)][str(season)][str(episode)]	
+
+		epid = cls.ep_imdb_ids[str(show_id)][str(season)][str(episode)]
 		url = 'https://www.imdb.com/title/{}/fullcredits'.format(epid)
 		req = requests.get(url)
 		soup = BeautifulSoup(req.content,'html.parser')
@@ -278,8 +257,7 @@ class APIClient():
 
 			cast_char = cast.find('td', {'class': 'character'}).text.replace('\n','')
 			#logger.info('Char: {}'.format(cast_char))
-			
+
 			episode_cast.append({str(cast_name):str(cast_char)})
 
 		return episode_cast
-
---
+++ tvapi/tvapi/structures.py
@@ -0,0 +1,176 @@
+import logging
+#logger = logging.get#logger('apilog')
+#
+#shows = {
+#    tmdb_id: {
+#        'imdb_id': imdb_id,
+#        'imdb_name': imdb_name,
+#        'tmdb_name': tmdb_name,
+#        'seasons': {
+#            season: {
+#                ep_id: {
+#                    'tmdb_id': tmdb_id,
+#                    'imdb_id': imdb_id,
+#                    'tmdb_name': tmdb_name,
+#                    'imdb_name': imdb_name,
+#                    'cast': {
+#                        actor: character,
+#                        ...
+#                    }
+#                },
+#                ...
+#            },
+#            ...
+#        }
+#    },
+#    ...
+#
+
+class Cast(dict):
+    def __init__(self):
+        super().__init__()
+
+    def get(self,actor=None):
+        if actor:
+            return self[str(actor)]
+        return self
+
+    def set(self,actor,character=None):
+        str_actor = str(actor)
+        str_char = str(character)
+        self.update({str_actor: str_char})
+        return self
+
+class Episode(dict):
+
+    def __init__(self):
+        super().__init__()
+
+    def get(self,tmdb_ep_id=None):
+        if tmdb_ep_id:
+            return self[str(tmdb_ep_id)]
+        return self
+
+    def set(self,ep_num,tmdb_id=None,imdb_id=None,tmdb_name=None,imdb_name=None,cast=None):
+        str_ep_num = str(ep_num)
+        str_tmdb_id = str(tmdb_id)
+        str_imdb_id = str(imdb_id)
+        str_tmdb_name = str(tmdb_name)
+        str_imdb_name = str(imdb_name)
+
+        self.update({
+            'ep_num': str(ep_num)
+            'tmdb_ep_id': str_tmdb_id,
+            'imdb_id': str_imdb_id,
+            'tmdb_name': str_tmdb_name,
+            'imdb_name': str_imdb_name,
+            'cast': cast,
+            })
+
+        return self
+
+
+class Season(dict):
+
+    def __init__(self):
+        super().__init__()
+
+    def get(self,ep_num=None):
+        if ep_num:
+            return self[str(ep_num)]
+        return self
+
+    def set(self,ep_num=None,episode=None,ep_cast=None):
+        str_ep_num = str(ep_num)
+
+        try:
+            self[str_ep_num]
+        except KeyError:
+            #logger.info('Episode Not Found: {}'.format(str_tmdb_ep_id))
+            self.update({str_ep_num: {}})
+
+        if episode:
+            self[str_ep_num] = episode
+            if ep_cast:
+                self[st_ep_num].set(cast=ep_cast)
+        else:
+            self[str_ep_num] = None
+
+        return self
+
+
+class Shows(dict):
+
+    def __init__(self):
+        super().__init__()
+
+    def set_episode_cast(self,show_id,season_num,ep_num,cast):
+        str_show_id = str(show_id)
+        str_season = str(season_num)
+        str_ep = str(ep_num)
+
+        try:
+            self[str_show_id]
+        except KeyError:
+            self.set(show_id)
+
+        print('TEST - {}'.format(self))
+
+        try:
+            self[str_show_id]['seasons'][str_season]
+        except KeyError:
+            self.add_season(str_show_id,str_season,Season())
+
+        self[str_show_id]['seasons'][str_season].set(str_ep,cast)
+
+        return cast
+
+    def set_episode(self,show_id,season_num,ep_num,episode=None,imdb_id=None,imdb_name=None):
+        str_show_id = str(show_id)
+        str_season = str(season_num)
+        str_ep = str(ep_num)
+        str_imdb_id = str(imdb_id)
+        str_imdb_name = str(imdb_name)
+
+        self[str_show_id]['seasons'][str_season][str_ep_num].set(ep_num,tmdb_id,tmdb_name,imdb_id,imdb_name)
+
+        return {str_ep: episode}
+
+    def add_season(self,show_id,season_num,season):
+        str_show_id = str(show_id)
+        str_season_num = str(season_num)
+
+        try:
+            self[str_show_id]['seasons'][str_season_num]
+        except KeyError:
+            self[str_show_id]['seasons'] = None
+            self[str_show_id]['seasons'].update({str_season_num: season})
+
+        self[str_show_id]['seasons'][str_season_num] = season
+
+        return season
+
+    def set(self,show_id,imdb_id=None,imdb_name=None,tmdb_name=None):
+        str_show_id = str(show_id)
+        str_imdb_id = str(imdb_id)
+        str_imdb_name = str(imdb_name)
+        str_tmdb_name = str(tmdb_name)
+
+        try:
+            self[str_show_id]
+        except KeyError:
+            #logger.info('No show found: {}'.format(str_show_id))
+            self.update({str_show_id: {}})
+
+        self[str_show_id].update({
+            'tmdb_id': str_tmdb_id,
+            'imdb_id': str_imdb_id,
+            'imdb_name': str_imdb_name,
+            'tmdb_name': str_tmdb_name,
+            'seasons': {},
+        })
+
+    def get(self,show_id=None):
+        if show_id:
+            return self[str(show_id)]
+        return self
