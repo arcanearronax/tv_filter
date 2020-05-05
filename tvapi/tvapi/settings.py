e8b8199fcbb2c0216c55691cd8efc9d0535e708c
arcanearronax <arcanearronax@aol.com>
2020-05-05 02:17:20 -0400

    Needed to implement a change to allow retrieval of the css files on the server.

--- tvapi/tvapi/settings.py
+++ tvapi/tvapi/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 #STATIC_ROOT = '/home/webmaster/sloth_proj/tvapi/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 
 LOGGING = {
